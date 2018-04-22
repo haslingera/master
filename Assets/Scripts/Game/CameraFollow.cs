4ea731635b1cfef543f2619061db64253aa41f2a
Andreas Haslinger <haslingerandreas@gmail.com>
2018-04-22 19:22:27 +0200

    Added rotation things

--- Assets/Scripts/Game/CameraFollow.cs
+++ Assets/Scripts/Game/CameraFollow.cs
@@ -1,27 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
 
 	public Transform target;
 
-	public float _smoothSpeed = 0.000005f;
+	public float SmoothSpeedRotation = 0.5f;
+	public float SmoothSpeedPosition = 0.5f;
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
 
-		Vector3 _desiredPosition = target.position;
-		Vector3 _smoothedPosition = Vector3.Lerp(transform.position, _desiredPosition, _smoothSpeed);
-		
-		Quaternion _desiredRotation= target.rotation;
-		Quaternion _smoothedRotation = Quaternion.Slerp(transform.rotation, _desiredRotation, _smoothSpeed);
+		Vector3 desiredPosition = target.position;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeedRotation);
 
-		transform.rotation = _smoothedRotation;
-		transform.position = _smoothedPosition;
+		Quaternion desiredRotation = target.rotation;
+		Quaternion smoothedRotation = Quaternion.Slerp(transform.rotation, desiredRotation, SmoothSpeedPosition);
 		
+		transform.rotation = smoothedRotation;
+		transform.position = smoothedPosition;
+		
+		Debug.Log(target.localRotation.eulerAngles);
+
+		/*Vector3 tmp = transform.localRotation.eulerAngles;
+		tmp.z = 0;
+		transform.localRotation = Quaternion.Euler(tmp);*/
+
 		//transform.LookAt(target);
 	}
 }
--- Assets/Scripts/Game/SpaceshipControls.cs
+++ Assets/Scripts/Game/SpaceshipControls.cs
@@ -6,43 +6,60 @@ namespace Game
 	public class SpaceshipControls : MonoBehaviour
 	{
 		
-		public float horizontalFactor = 2f;
-		public float verticalFactor = 2f;
+		[Header("Rotation")]
+		public float HorizontalTurnFactor = 2f;
+		public float VerticalTurnFactor = 2f;
+		public float LeanTurnFactor = 2f;
+		[Header("Acceleration")]
+		public float Acceleration = 1f;
+		[Header("Boost")]
+		public float Boost = 1f;
+		[Header("Smoothing")]
+		public float AccelerationSmoothing = 0.5f;
+		public float RotationSmoothing = 0.5f;
 		
 		private float _screenWidth;
 		private float _screenHeight;
-		private float _screenRatio;
+		private Vector3 _targetPosition;
+		private Vector3 _targetRotation;
 		private Vector3 _centeredNormalizedMousePosition;
-		private Rigidbody _rigidbody;
 	
 		void Start()
 		{
 			_screenWidth = Screen.width;
 			_screenHeight = Screen.height;
-			_screenRatio =_screenWidth / _screenHeight;
-			_rigidbody = transform.GetComponent<Rigidbody>();
-			_rigidbody.maxAngularVelocity = 1f;
+			
+			_targetPosition = transform.position;
+			_targetRotation = transform.rotation.eulerAngles;
 		}
 
 		void FixedUpdate()
 		{
-
+			
+			CenterSquaredAndNormalizeMousePosition();
+			
 			if (Input.GetKey(KeyCode.W))
 			{
-				CenterSquaredAndNormalizeMousePosition();
-				//Quaternion rotationVector = gameObject.transform.rotation * Quaternion.Euler(-_centeredNormalizedMousePosition.y * verticalFactor, _centeredNormalizedMousePosition.x * horizontalFactor, 0);
-				//gameObject.transform.rotation *= Quaternion.Euler(-_centeredNormalizedMousePosition.y * verticalFactor, _centeredNormalizedMousePosition.x * horizontalFactor, 0);
-				_rigidbody.AddTorque(new Vector3(-_centeredNormalizedMousePosition.y * verticalFactor, _centeredNormalizedMousePosition.x * horizontalFactor, 0f));
-				_rigidbody.AddForce(transform.forward * 3);
+				_targetRotation.x += -_centeredNormalizedMousePosition.y * VerticalTurnFactor;
+				_targetRotation.y += _centeredNormalizedMousePosition.x * HorizontalTurnFactor;
+				_targetRotation.z = -_centeredNormalizedMousePosition.x * LeanTurnFactor;
+				
+				_targetPosition += transform.forward * Acceleration;
+				
+				if (Input.GetKey(KeyCode.LeftShift))
+				{
+					_targetPosition += transform.forward * Boost;
+				}
 			}
+			
+			transform.position = Vector3.Lerp(transform.position, _targetPosition, AccelerationSmoothing);
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(_targetRotation), RotationSmoothing);
 			
 		}
 		
 		void CenterSquaredAndNormalizeMousePosition ()
-		{
-			Debug.Log(_screenRatio);
-			
-			_centeredNormalizedMousePosition.x = Mathf.Clamp((Input.mousePosition.x / _screenWidth - 0.5f) * _screenRatio, -0.5f, 0.5f);
+		{			
+			_centeredNormalizedMousePosition.x = Mathf.Clamp(Input.mousePosition.x / _screenWidth - 0.5f, -0.5f, 0.5f);
 			_centeredNormalizedMousePosition.y = Mathf.Clamp(Input.mousePosition.y / _screenHeight - 0.5f, -0.5f, 0.5f);
 			
 			_centeredNormalizedMousePosition.x = _centeredNormalizedMousePosition.x < 0 ? -(float) Math.Pow(_centeredNormalizedMousePosition.x, 2) :(float) Math.Pow(_centeredNormalizedMousePosition.x, 2);
